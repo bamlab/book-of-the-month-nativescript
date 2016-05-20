ace617407e4aebddd6078ad55453e465159d3548
Florian Rival <Florian.Rival@gmail.com>
2016-05-20 13:01:45 +0200

    Remove useless thing

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -6,7 +6,6 @@ import {View} from "ui/core/view";
 import {prompt} from "ui/dialogs";
 import {Page} from "ui/page";
 import {TextField} from "ui/text-field";
-import {User} from "../../shared/user/user";
 import {UserService} from "../../shared/user/user.service";
 import {setHintColor} from "../../utils/hint-util";
 import {alert} from "../../utils/dialog-util";
@@ -19,103 +18,31 @@ import webViewModule = require("ui/web-view");
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
 })
 export class LoginPage implements OnInit {
-  user: User;
   isLoggingIn = true;
   isAuthenticating = false;
 
-  @ViewChild("initialContainer") initialContainer: ElementRef;
-  @ViewChild("mainContainer") mainContainer: ElementRef;
-  @ViewChild("logoContainer") logoContainer: ElementRef;
-  @ViewChild("formControls") formControls: ElementRef;
-  @ViewChild("signUpStack") signUpStack: ElementRef;
-  @ViewChild("email") email: ElementRef;
-  @ViewChild("password") password: ElementRef;
+  @ViewChild("webView") webView;
 
   constructor(private _router: Router,
     private _userService: UserService,
     private page: Page) {
-    this.user = new User();
-    this.user.email = "user@nativescript.org";
-    this.user.password = "password";
   }
 
   ngOnInit() {
     this.page.actionBarHidden = true;
-  }
 
-  focusPassword() {
-    this.password.nativeElement.focus();
-  }
+    this.webView.on(webViewModule.WebView.loadFinishedEvent, function (args: webViewModule.LoadEventData) {
+        console.log('Load finished');
 
-  submit() {
-    if (!this.user.isValidEmail()) {
-      alert("Enter a valid email address.");
-      return;
-    }
-
-    this.isAuthenticating = true;
-    if (this.isLoggingIn) {
-      this.login();
-    } else {
-      this.signUp();
-    }
-  }
-
-  login() {
-    this._userService.login(this.user)
-      .subscribe(
-        () => {
-          this.isAuthenticating = false;
-          this._router.navigate(["List"]);
-        },
-        (error) => {
-          alert("Unfortunately we could not find your account.");
-          this.isAuthenticating = false;
+        let message;
+        if (!args.error) {
+            message = "WebView finished loading " + args.url;
         }
-      );
-  }
-
-  signUp() {
-    this._userService.register(this.user)
-      .subscribe(
-        () => {
-          alert("Your account was successfully created.");
-          this.isAuthenticating = false;
-          this.toggleDisplay();
-        },
-        () => {
-          alert("Unfortunately we were unable to create your account.");
-          this.isAuthenticating = false;
+        else {
+            message = "Error loading " + args.url + ": " + args.error;
         }
-      );
-  }
 
-  forgotPassword() {
-    prompt({
-      title: "Forgot Password",
-      message: "Enter the email address you used to register for Groceries to reset your password.",
-      defaultText: "",
-      okButtonText: "Ok",
-      cancelButtonText: "Cancel"
-    }).then((data) => {
-      if (data.result) {
-        this._userService.resetPassword(data.text.trim())
-          .subscribe(() => {
-            alert("Your password was successfully reset. Please check your email for instructions on choosing a new password.");
-          }, () => {
-            alert("Unfortunately, an error occurred resetting your password.");
-          });
-      }
-    });
-  }
-
-  toggleDisplay() {
-    this.isLoggingIn = !this.isLoggingIn;
-    this.setTextFieldColors();
-    let mainContainer = <View>this.mainContainer.nativeElement;
-    mainContainer.animate({
-      backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
-      duration: 200
+        console.log(message);
     });
   }
 
@@ -124,51 +51,5 @@ export class LoginPage implements OnInit {
       scale: { x: 1.2, y: 1.2 },
       duration: 8000
     });
-  }
-
-  showMainContent() {
-    let initialContainer = <View>this.initialContainer.nativeElement;
-    let mainContainer = <View>this.mainContainer.nativeElement;
-    let logoContainer = <View>this.logoContainer.nativeElement;
-    let formControls = <View>this.formControls.nativeElement;
-    let signUpStack = <View>this.signUpStack.nativeElement;
-    let animations = [];
-
-    // Fade out the initial content over one half second
-    initialContainer.animate({
-      opacity: 0,
-      duration: 500
-    }).then(function() {
-      // After the animation completes, hide the initial container and
-      // show the main container and logo. The main container and logo will
-      // not immediately appear because their opacity is set to 0 in CSS.
-      initialContainer.style.visibility = "collapse";
-      mainContainer.style.visibility = "visible";
-      logoContainer.style.visibility = "visible";
-
-      // Fade in the main container and logo over one half second.
-      animations.push({ target: mainContainer, opacity: 1, duration: 500 });
-      animations.push({ target: logoContainer, opacity: 1, duration: 500 });
-
-      // Slide up the form controls and sign up container.
-      animations.push({ target: signUpStack, translate: { x: 0, y: 0 }, opacity: 1, delay: 500, duration: 150 });
-      animations.push({ target: formControls, translate: { x: 0, y: 0 }, opacity: 1, delay: 650, duration: 150 });
-
-      // Kick off the animation queue
-      new Animation(animations, false).play();
-    })
-  }
-
-  setTextFieldColors() {
-    let emailTextField = <TextField>this.email.nativeElement;
-    let passwordTextField = <TextField>this.password.nativeElement;
-
-    let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
-    emailTextField.color = mainTextColor;
-    passwordTextField.color = mainTextColor;
-
-    let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
-    setHintColor({ view: emailTextField, color: hintColor });
-    setHintColor({ view: passwordTextField, color: hintColor });
   }
 }
